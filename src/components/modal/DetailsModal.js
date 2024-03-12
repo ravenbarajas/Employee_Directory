25b95ce8f8a1428f92834e15f3f98914820de642
Carl Raven Barajas <56469543+ravenbarajas@users.noreply.github.com>
2024-03-12 14:20:29 +0800

    Updated Frontend: Removed Filter Checkbox. Updated Details Modal

--- src/components/modal/DetailsModal.js
+++ src/components/modal/DetailsModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/DetailsModal.css'; // Import the stylesheet
+import placeholderImage from '../assets/employee-profilepic.png';
 
 const DetailsModal = ({ employee, onClose }) => {
   // You can customize the content and styling of the details modal here
@@ -16,33 +17,43 @@ const DetailsModal = ({ employee, onClose }) => {
       <div className="dm-content">
         <div className="dm-content-1strow">
           <div className="dm-content-1strow-1stcol">
-            <p><strong>Employee ID:</strong> {employee.empID}</p>
+            <div className='employee-profilepic'>
+              <img src={placeholderImage}/>
+            </div>
+          </div>
+          <div className="dm-content-1strow-2ndcol">
             <p><strong>Employee Name:</strong> {employee.empName}</p>
+            <p><strong>Department:</strong> {employee.empDept}</p>
+            <p><strong>Position:</strong> {employee.empPosition}</p>
+          </div>
+          <div className="dm-content-1strow-3rdcol">
+            <p><strong>Department ID:</strong> {employee.empDeptID}</p>
+            <p><strong>Date of Hire:</strong> {employee.empDateofhire}</p>
+          </div>
+        </div>
+        <div className="dm-content-2ndrow">
+          <div className="dm-content-2ndrow-1stcol">
+            <p><strong>Employee ID:</strong> {employee.empID}</p>
             <p><strong>Date of Birth:</strong> {employee.empBday}</p>
             <p><strong>Gender:</strong> {employee.empGender}</p>
           </div>
-          <div className="dm-content-1strow-2ndcol">
+          <div className="dm-content-2ndrow-2ndcol">
             <p><strong>Marital Status:</strong> {employee.empMaritalstatus}</p>
             <p><strong>Nationality:</strong> {employee.empNationality}</p>
             <p><strong>Religion:</strong> {employee.empReligion}</p>
           </div>
         </div>
-
-        <div className="dm-content-2ndrow">
-          <div className="dm-content-2ndrow-1stcol">
-            <p><strong>Department ID:</strong> {employee.empDeptID}</p>
-            <p><strong>Department:</strong> {employee.empDept}</p>
-            <p><strong>Position:</strong> {employee.empPosition}</p>
-            <p><strong>Date of Hire:</strong> {employee.empDateofhire}</p>
-          </div>
-          <div className="dm-content-2ndrow-2ndcol">
+        <div className="dm-content-3rdrow">
+          <div className="dm-content-3rdrow-1stcol">
             <p><strong>TIN ID:</strong> {employee.empTinID}</p>
             <p><strong>HDMF ID:</strong> {employee.empHdmfID}</p>
+          </div>
+          <div className="dm-content-3rdrow-2ndcol">
             <p><strong>Philhealth ID:</strong> {employee.empPhilhealthID}</p>
             <p><strong>SSS ID:</strong> {employee.empSssID}</p>
           </div>
         </div>
-        <div className="dm-content-3rdrow">
+        <div className="dm-content-4throw">
           <p><strong>Emergency Contact Name:</strong> {employee.empEMRGNCname}</p>
           <p><strong>Emergency Contact Relationship:</strong> {employee.empEMRGNCrelationship}</p>
           <p><strong>Emergency Contact Phone Number:</strong> {employee.empEMRGNCphonenum}</p>
